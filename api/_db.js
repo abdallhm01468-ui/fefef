2eb282776661d6dee3c229600ae156980f255d04
Abdallah Mohamed <abdallhm01468@gmail.com>
2025-12-30 00:07:02 +0200

    Fix frontend routing for Vercel

---
+++ api/_db.js
@@ -0,0 +1,24 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri = process.env.MONGODB_URI;
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+  retryWrites: true,
+  w: "majority",
+};
+
+let client;
+let clientPromise;
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri, options);
+  global._mongoClientPromise = client.connect();
+}
+
+clientPromise = global._mongoClientPromise;
+
+export default clientPromise;
